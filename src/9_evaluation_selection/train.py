9a85f9140890dac4dfe54e96aea45a64c0d882a0
ml_user <ml_user@example.com>
2022-05-11 10:53:23 +0300

    create structure and add specific files

---
+++ src/9_evaluation_selection/train.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import click
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/heart.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    random_state: int,
+    test_split_ratio: float,
+    use_scaler: bool,
+    max_iter: int,
+    logreg_c: float,
+) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+    )
