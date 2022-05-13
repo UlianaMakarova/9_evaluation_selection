426fae22e817d47b7737c2aba659772ed0e255d5
UlianaMakarova <49311061+UlianaMakarova@users.noreply.github.com>
2022-05-13 16:34:31 +0300

    Add files via upload

--- src/9_evaluation_selection/data_reader.py
+++ src/9_evaluation_selection/data_reader.py
@@ -7,13 +7,23 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def get_dataset(
-    csv_path: Path, random_state: int, test_split_ratio: float
+    csv_path: Path, split_need: bool, random_state: int, test_split_ratio: float
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    dataset_train = pd.read_csv(csv_path)
-    click.echo(f"Dataset shape: {dataset_train.shape}.")
-    features = dataset_train.drop("Cover_Type", axis=1)
-    target = dataset_train["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=test_split_ratio, random_state=random_state
-    )
-    return features_train, target_train, features_val, target_val
+    dataset = pd.read_csv(csv_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
+    target = None
+    if "Cover_Type" in dataset:
+        features = dataset.drop("Cover_Type", axis=1)
+        target = dataset["Cover_Type"]
+    else:
+        features = dataset
+
+    if split_need:
+        features_train, features_val, target_train, target_val = train_test_split(
+            features, target,
+            test_size=test_split_ratio,
+            random_state=random_state
+            )
+        return features_train, target_train, features_val, target_val
+    else:
+        return features, target
--- src/9_evaluation_selection/train.py
+++ src/9_evaluation_selection/train.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from joblib import dump
+from joblib import dump, load
+import pandas as pd
 
 import click
 
@@ -34,6 +35,12 @@ from .pipeline import create_pipeline_kneighborsclassifier
     show_default=True,
 )
 #split dataset
+@click.option(
+    "--split-need",
+    default=True,
+    type=bool,
+    show_default=True,
+)
 @click.option(
     "--test-split-ratio",
     default=0.2,
@@ -89,6 +96,7 @@ from .pipeline import create_pipeline_kneighborsclassifier
 def train(
     dataset_path: Path,
     save_model_path: Path,
+    split_need: bool,
     random_state: int,
     test_split_ratio: float,
     use_scaler: bool,
@@ -99,11 +107,19 @@ def train(
     algorithm: str,
     n_jobs: int,
 ) -> None:
-    features_train, target_train, features_val, target_val = get_dataset(
-        dataset_path,
-        random_state,
-        test_split_ratio,
-    )
+    if split_need:
+        features_train, target_train, features_val, target_val = get_dataset(
+                        dataset_path,
+                        split_need,
+                        random_state,
+                        test_split_ratio,
+                        )
+    else:
+        features_train, target_train = get_dataset(
+                        dataset_path,
+                        split_need,
+                        )
+
     click.echo(f"Features_train shape: {features_train.shape}.")
     click.echo(f"Target_train shape: {target_train.shape}.")
     with mlflow.start_run():
@@ -115,12 +131,55 @@ def train(
         mlflow.log_param("logreg_c", logreg_c)
         mlflow.log_metric("accuracy", accuracy_logisticregression)
         click.echo(f"Accuracy: {accuracy_logisticregression}.")
-        dump(pipeline1, save_model_path)
-        click.echo(f"Model is saved to {save_model_path}.")
+        #dump(pipeline1, save_model_path)
+        #click.echo(f"Model is saved to {save_model_path}.")
         pipeline2 = create_pipeline_kneighborsclassifier(n_neighbors, weights, algorithm, n_jobs)
+
         pipeline2.fit(features_train, target_train)
         accuracy_kneighborsclassifier = accuracy_score(target_val, pipeline2.predict(features_val))
         click.echo(f"Accuracy: {accuracy_kneighborsclassifier}.")
+        dump(pipeline2, save_model_path)
+        click.echo(f"Model is saved to {save_model_path}.")
 
+@click.command()
+@click.option(
+    "-d",
+    "--test-dataset-path",
+    default="data/test.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-d",
+    "--model-path",
+    default="data/results/model.joblib",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+
+
+def save_competiotion_results(
+        test_dataset_path: Path,
+        model_path: Path
+) -> None:
+
+    #features, target = get_dataset(test_dataset_path, False, None, None)
+    features = pd.read_csv(test_dataset_path)
+
+    #features = pd.DataFrame(features)
+    model = load(model_path)
+    click.echo(f"model: {model}")
+
+    click.echo(f"features shape: {features.shape}")
+
+    y_pred = model.predict(features)
+
+    #predictions_test = pd.DataFrame(y_pred)
+    #predictions_test.columns = ['Cover_Type']
+
+    #result = pd.concat([features["Id"], predictions_test], axis=1)
+
+    #click.echo(f"y_pred shape: {y_pred.shape}")
+    #result.to_csv('save_model_path', index=False)
 
 
